a666d020ae33faf9a31a7022f87a416d9052acec
Guto Xavier <gutoffline@gmail.com>
2025-02-21 15:22:09 -0300

    correção no nome da função

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 document.getElementById("nome").addEventListener("blur", function(){
     fnAdicionarMensagemDeErro("mensagem-erro-nome", "limpar")
-    
+
     let nomevalido = fnValidarMinimoDeCaracteres(3, this.value);   
     if(nomevalido == false){
         fnAdicionarMensagemDeErro("mensagem-erro-nome", "mínimo 3 caracteres")
     }
 
-    let nomeobrigatorio = fnValidarCampoObriatorio(this.value);
+    let nomeobrigatorio = fnValidarCampoObrigatorio(this.value);
     if(nomeobrigatorio == false){
         fnAdicionarMensagemDeErro("mensagem-erro-nome", "campo obrigatório")
     }
